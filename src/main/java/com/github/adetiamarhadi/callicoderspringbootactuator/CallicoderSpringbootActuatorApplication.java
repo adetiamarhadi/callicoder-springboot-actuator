20133d4a97add1f8da2c2d1f3782df582a1611ad
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-16 10:12:54 +0700

    init project

---
+++ src/main/java/com/github/adetiamarhadi/callicoderspringbootactuator/CallicoderSpringbootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.callicoderspringbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallicoderSpringbootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallicoderSpringbootActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/adetiamarhadi/callicoderspringbootactuator/CallicoderSpringbootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.callicoderspringbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallicoderSpringbootActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
